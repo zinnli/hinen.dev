5c583e999819cb250fb32a106f4d417c0a4a0963
zinnli <limgx5192@gmail.com>
2025-04-20 22:13:27 +0900

    style: post 상세조회 style 수정 (#1)

--- src/app/post/[category]/[slug]/page.tsx
+++ src/app/post/[category]/[slug]/page.tsx
@@ -9,8 +9,10 @@ const PostDetail = async ({ params }: Props) => {
   const post = await getPostDetail(params.category, params.slug);
 
   return (
-    <article className="flex flex-col w-4/5 max-w-[700px] py-10">
-      <h2 className="text-primary text-36 font-bold mb-10">{post.title}</h2>
+    <article className="flex flex-col w-[100%] max-w-[700px] px-4 py-10">
+      <h2 className="flex justify-center w-[100%] text-primary text-40 font-bold mb-10">
+        {post.title}
+      </h2>
       <span className="mb-8 text-right">{post.date}</span>
       <Post post={post} />
     </article>
