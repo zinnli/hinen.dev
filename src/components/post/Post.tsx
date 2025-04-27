9da28b3a2b46a96c31eaa0b2d5ea4ef98fbcd914
zinnli <limgx5192@gmail.com>
2025-04-27 21:03:13 +0900

    style: slug 페이지 width 수정 (#7)

--- src/components/post/Post.tsx
+++ src/components/post/Post.tsx
@@ -20,7 +20,7 @@ const Post = ({ post }: Props) => {
   }
 
   return (
-    <article className="prose  prose-lg prose-slate">
+    <article className="w-[100%] prose prose-lg prose-slate">
       <MDXRemote
         source={post.content}
         options={{
