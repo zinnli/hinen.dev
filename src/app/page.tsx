65714721f6668c45fdb643d2f414cf8efc358828
zinnli <102575747+zinnli@users.noreply.github.com>
2025-04-27 18:25:53 +0900

    about 관련 스크린추가 (#6)
    * style: 불요 ㅁcss, 개행 삭제 및 font-size 추가 (#5)
    
    * feat: home -> blog로 변경 및 메인 이미지 추가 (#5)
    
    * feat: about 페이지 템플릿 추가 (#5)
    
    * style: 전반적인 style 수정 (#5)
     - title 삭제
     - 전반적인 font-size 수정
     - profile 이미지 교체 및 추가
    
    * feat: 적절한 태그로 변경 (#5)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,58 @@
+import Image from "next/image";
+import Link from "next/link";
+
 import { List } from "@/components";
 import { getPostList } from "@/lib/mdx";
+import ProfileImage from "@/assets/images/profile.png";
 
-const Home = async () => {
+const Blog = async () => {
   const post = await getPostList("");
 
   return (
     <main className="flex flex-col items-center w-[100%] max-w-[750px] px-7">
-      <div className="w-[100%] h-[250px] bg-primary_sub m-6 mt-12">
-        자기소개
+      <div className="relative w-[100%] h-[150px] m-6 mt-12">
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col z-50">
+          <span className="text-22 text-gray_bg font-black text-right">
+            WEB FRONTEND DEVELOPER
+          </span>
+          <span className="text-[80px] text-gray_bg italic font-black text-left leading-none z-50">
+            ZINLOG
+          </span>
+        </div>
+        {/* NOTE: bg 이미지 고민중 */}
+        <Image
+          className="w-[100%] h-[100%] bg-gray object-cover grayscale-[0.2]"
+          src={ProfileImage}
+          alt="profile"
+        />
       </div>
-      <h1 className="flex justify-start gap-x-5 w-[100%] text-50 text-primary my-7 mb-4">
-        <span>Recent</span>
-        <span className="font-black">Posts</span>
-      </h1>
-      <div className="flex flex-col gap-y-2">
-        {post.map((item) => {
-          return (
-            <List
-              key={item.filePath}
-              isPost={false}
-              category={item.categoryPath}
-              date={item.date}
-              desc={item.desc}
-              path={`/post/${item.filePath}`}
-              title={item.title}
-            />
-          );
-        })}
-      </div>
+      {/* NOTE: 경력 기술서 - 노션링크 추가 예정 */}
+      <Link href="" className="hover:text-primary">
+        &gt; more about me
+      </Link>
+      <section>
+        <h1 className="flex justify-start gap-x-5 w-[100%] mt-8 my-5 mb-3 text-36 text-black">
+          <span>Recent</span>
+          <span className="font-black">Posts</span>
+        </h1>
+        <div className="flex flex-col gap-y-2">
+          {post.slice(0, 3).map((item) => {
+            return (
+              <List
+                key={item.filePath}
+                isPost={false}
+                category={item.categoryPath}
+                date={item.date}
+                desc={item.desc}
+                path={`/post/${item.filePath}`}
+                title={item.title}
+              />
+            );
+          })}
+        </div>
+      </section>
     </main>
   );
 };
 
-export default Home;
+export default Blog;
--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -20,11 +20,8 @@ const Page = async ({ params, searchParams }: Props) => {
     `text-16 ${isSelected ? "font-bold text-black" : "font-normal text-black/80"}`;
 
   return (
-    <section className="flex flex-col items-center w-[100%] max-w-[750px] px-7">
-      <h1 className="flex justify-start w-[100%] text-50 font-black text-primary my-7 mb-4">
-        POSTS
-      </h1>
-      <div className="flex justify-end gap-x-[10px] w-[100%] mb-12">
+    <main className="flex flex-col items-center w-[100%] max-w-[750px] px-7">
+      <div className="flex justify-start gap-x-[10px] w-[100%]  mt-12 mb-12">
         <Link className={selectTextStyle(!selectedCategory)} href={"/post"}>
           All({totalCount})
         </Link>
@@ -40,7 +37,7 @@ const Page = async ({ params, searchParams }: Props) => {
           );
         })}
       </div>
-      <div className="flex flex-col gap-y-2">
+      <section className="flex flex-col gap-y-2">
         {(selectedCategory
           ? post.filter((item) => item.categoryPath === selectedCategory)
           : post
@@ -57,8 +54,8 @@ const Page = async ({ params, searchParams }: Props) => {
             />
           );
         })}
-      </div>
-    </section>
+      </section>
+    </main>
   );
 };
 
