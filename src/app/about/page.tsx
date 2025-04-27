1e829a78518e2124ad90b855baea3d0d1e421fbe
zinnli <limgx5192@gmail.com>
2025-04-27 18:18:25 +0900

    feat: about 페이지 템플릿 추가 (#5)

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,31 @@
-export default async function Page() {
-  return <></>;
-}
+import Image from "next/image";
+
+import ProfileImage from "@/assets/images/about-profile.jpeg";
+
+const About = () => {
+  return (
+    <main className="flex flex-col items-center w-[100%] max-w-[750px] px-7">
+      <section className="flex flex-col mt-12">
+        <div className="flex justify-start gap-x-5 w-[100%] h-[100px] mb-10">
+          <Image
+            className="w-[100px] h-[100px] rounded-full bg-gray object-cover"
+            src={ProfileImage}
+            alt="profile"
+          />
+          <div className="flex flex-col justify-between py-[5px]">
+            <div className="flex gap-x-2 text-30 font-bold text-primary">
+              <span>이현진</span>
+              <span>LEE HYUNJIN</span>
+            </div>
+            <span className=" text-22 text-primary_sub">
+              Frontend developer
+            </span>
+          </div>
+        </div>
+        <span>아름다운 UI를 사랑하고 구현해내는데서 행복을 느낍니다.</span>
+      </section>
+    </main>
+  );
+};
+
+export default About;
