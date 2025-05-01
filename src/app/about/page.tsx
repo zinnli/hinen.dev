8ec81e60b22c0b1cf8d9e76778ffbec1bed1b445
zinnli <limgx5192@gmail.com>
2025-05-01 20:44:25 +0900

    feat: about 이미지 삭제(#10)

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,4 @@
-import Image from "next/image";
 import type { Metadata } from "next";
-
-import ProfileImage from "@/assets/images/about-profile.jpeg";
 
 export const metadata: Metadata = {
   title: "ZINLOG | ABOUT",
@@ -10,27 +7,18 @@ export const metadata: Metadata = {
 
 const About = () => {
   return (
-    <main className="flex flex-col items-center w-[100%] max-w-[800px] px-7">
+    <>
       <section className="flex flex-col mt-12 w-[100%]">
-        <div className="flex justify-start gap-x-5 w-[100%] h-[100px] mb-10">
-          <Image
-            className="w-[100px] h-[100px] rounded-full bg-gray object-cover"
-            src={ProfileImage}
-            alt="profile"
-          />
-          <div className="flex flex-col justify-start py-[5px]">
-            <div className="flex gap-x-2 text-20 font-bold text-primary">
-              <span>이현진</span>
-              <span>LEE HYUNJIN</span>
-            </div>
-            <span className=" text-18 text-primary_sub">
-              Frontend developer
-            </span>
+        <div className="flex flex-col justify-start py-[5px] mb-10">
+          <div className="flex gap-x-2 text-20 font-bold text-primary">
+            <span>이현진</span>
+            <span>LEE HYUNJIN</span>
           </div>
+          <span className=" text-18 text-primary_sub">Frontend developer</span>
         </div>
         <span>아름다운 UI를 사랑하고 구현해내는데서 행복을 느낍니다.</span>
       </section>
-    </main>
+    </>
   );
 };
 
